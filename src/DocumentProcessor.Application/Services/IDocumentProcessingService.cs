cc0e1fa185c0329f50e789e65a14490f1bfd4c96
Ty Augustine <tyronea_2000@yahoo.com>
2025-11-28 14:38:42 -0500

    reset

--- src/DocumentProcessor.Application/Services/IDocumentProcessingService.cs
+++
@@ -1,9 +0,0 @@
-using DocumentProcessor.Core.Interfaces;
-
-namespace DocumentProcessor.Application.Services;
-
-public interface IDocumentProcessingService
-{
-    Task<Guid> QueueDocumentForProcessingAsync(Guid documentId);
-    Task<DocumentProcessingResult> ProcessDocumentAsync(Guid documentId, AIProviderType? providerType = null);
-}
